1225dfa1d0ed26abd5b8d79dae2d555ce7356c4a
amandarangel <50452552+amandarangel@users.noreply.github.com>
2019-10-24 16:22:00 -0300

    inclusão do navbar no appcontainer

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -4,8 +4,8 @@ import Footer from "./Footer/Footer";
 import ProductPage from "./ProductPage/ProductPage";
 import { Grid } from "@material-ui/core";
 import styled from "styled-components";
-import React from 'react'
 import CardsGrid from './CardsGrid/CardsGrid'
+import NavbarFilter from './NavbarFilter/NavbarFilter'
 
 
 const StyledGrid = styled(Grid)`
@@ -21,6 +21,7 @@ export class AppContainer extends React.Component {
     return (
       <div>
         <Header />
+				<NavbarFilter />
         <StyledGrid container justify="center" spacing={0}>
           <Grid item xs={12} sm={12} lg={8}>
 						<CardsGrid />	
